becc71270b926a8b1e939e152e34d2c134ed957c
Darren Harvey <darren.harvey@me.com>
2019-05-08 08:56:41 +0100

    Add data source for content

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,22 +1,23 @@
 <template>
-  <Page
-    class="contact"
-    title="Get in touch"
-  >
-    Rah
+  <Page class="contact" title="Get in touch">
+    <Card v-html="content"></Card>
   </Page>
 </template>
 
 
 <script>
-import Card from '../components/Card.vue';
-import Page from '../components/Page.vue';
+import { contact } from "../../data.json";
+import Card from "../components/Card.vue";
+import Page from "../components/Page.vue";
 
 export default {
-  name: 'contact',
+  name: "contact",
+  data() {
+    return { content: contact.content.join("\n") };
+  },
   components: {
     Card,
-    Page,
-  },
+    Page
+  }
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <Page class="home" subtitle="Hello, I'm" title="Darren Harvey" isGrid>
-    <router-link class="dh-card__link" v-for="page in pages" :to="page.href" :key="page.name">
+    <router-link class="dh-card__link" v-for="page in pages" :to="'/' + page.id" :key="page.name">
       <Card :title="page.name" :icon="page.icon">
         <div>{{page.content}}</div>
       </Card>
@@ -9,6 +9,7 @@
 </template>
 
 <script>
+import { home } from "../../data.json";
 import Card from "../components/Card.vue";
 import Page from "../components/Page.vue";
 
@@ -16,14 +17,7 @@ export default {
   name: "home",
   data() {
     return {
-      pages: [
-        {
-          href: '/photography',
-          name: 'Photography',
-          content: 'Some snaps',
-          icon: 'aperture',
-        },
-      ]
+      pages: home.pages,
     };
   },
   components: {
