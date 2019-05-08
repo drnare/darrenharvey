aa4f20a7bddd68ad5d9155620c0a56763bcac247
Darren Harvey <darren.harvey@me.com>
2019-05-08 08:57:29 +0100

    Add NotFound view

---
+++ src/views/Not-Found.vue
@@ -0,0 +1,22 @@
+<template>
+  <Page
+    class="not-found"
+    title="Not Found"
+  >
+    <p>Nothing here.</p>
+  </Page>
+</template>
+
+
+<script>
+import Card from '../components/Card.vue';
+import Page from '../components/Page.vue';
+
+export default {
+  name: 'contact',
+  components: {
+    Card,
+    Page,
+  },
+};
+</script>
